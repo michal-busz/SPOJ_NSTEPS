44eed7661721e2cf53426f8a4ce3fda60d7c077b
Michal Busz <michal@busz.it>
2019-03-28 20:04:02 +0000

    completed

--- src/it/busz/Main.java
+++ src/it/busz/Main.java
@@ -1,8 +1,37 @@
 package it.busz;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        int num = Integer.parseInt(input.nextLine());
+        for(;num>0;num--){
+            String txt = input.nextLine();
+            String []temp = txt.split(" ");
+            System.out.println(getNumber(Integer.parseInt(temp[0]),Integer.parseInt(temp[1])));
+        }
+    }
+
+    private static String getNumber(int x, int y){
+        if(x-y==2)
+        {
+            return Integer.toString(calculate(2,y));
+        } else if (x == y) {
+            return Integer.toString(calculate(0,x));
+        }
+        else
+            return "No Number";
+    }
+    private static int calculate(int base, int steps)
+    {
+        for(int i=0;i<steps;i++)
+        {
+            if(i%2==0)
+                base+=1;
+            else
+                base+=3;
+        }
+        return base;
     }
 }
